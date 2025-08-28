1907898d338831dd408d63dbd7e41ffa94faca65
ThanhLong0310 <long.lt0310@gmail.com>
2025-08-28 09:01:54 +0700

    finish

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   message: string = "Xin chào! Đây là Home Component.";
   counter: number = 0;
+  public traiCay =['cam','quy','mit','dua','canh','tao','oi','xoai','nhan'];
+  public traiCay1 =[{ten:'táo',gia:12},{ten:'quýt',gia:1},{ten:'cà',gia:-1},{ten:'bưởi',gia:-19},{ten:'mận',gia:32},{ten:'lê',gia:42},{ten:'quả',gia:-12},{ten:'mận',gia:42},{ten:'quả',gia:-90},{ten:'mận',gia:-4}]
 
   increase() {
     this.counter++;
