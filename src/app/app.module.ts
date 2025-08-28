5ad54a7d2026a6836d2acc9c68d06b81a6e289ca
ThanhLong0310 <long.lt0310@gmail.com>
2025-08-28 09:58:34 +0700

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component'; // thêm dòng này
+import { HomeComponent } from './home/home.component';
+import { NaviComponent } from './navi/navi.component'; // thêm dòng này
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent   // thêm dòng này
+    HomeComponent,
+    NaviComponent   // thêm dòng này
   ],
   imports: [
     BrowserModule
